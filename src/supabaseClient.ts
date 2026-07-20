f8c583eb03f6a927858eed0057daad3be787e066
iatelecimento-lab <ia.telecimento@gmail.com>
2026-07-20 19:55:52 +0000

    Fix supabase credentials fallback

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://wzgqlkfisrhgekshysks.supabase.co';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_ejvCUEWR2mPsnOPtlQruBg_sdg7TnvV';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
