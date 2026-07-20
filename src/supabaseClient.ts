b64ab540ff7f13debf8725f00199a9d1d4b23b45
iatelecimento-lab <ia.telecimento@gmail.com>
2026-07-20 17:53:31 -0300

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://wzgqlkfisrhgekshysks.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_ejvCUEWR2mPsnOPtlQruBg_sdg7TnvV';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
